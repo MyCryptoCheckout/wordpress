37a7896e598424cac8ceb9dc5cd78e97a0b6a47e
Edward Plainview <edward@plainview.se>
2018-01-09 21:25:15 +0100

    ETH use 14 decimals not 18.

--- src/currencies/ETH.php
+++ src/currencies/ETH.php
@@ -15,7 +15,9 @@ class ETH
 	**/
 	public function get_decimal_precision()
 	{
-		return 18;
+		// It actually has 18, but some wallets will unnecessarily round such small amounts off, making payments impossible.
+		// 14 seems to work much better.
+		return 14;
 	}
 
 	/**
