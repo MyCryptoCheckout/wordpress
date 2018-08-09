b0189241c0bd4b4b7205985140ec198809784c98
Edward Plainview <edward@plainview.se>
2018-08-09 22:44:44 +0200

    Add XEM icons

--- src/ecommerce/woocommerce/WC_Gateway_MyCryptoCheckout.php
+++ src/ecommerce/woocommerce/WC_Gateway_MyCryptoCheckout.php
@@ -75,6 +75,7 @@ class WC_Gateway_MyCryptoCheckout extends \WC_Payment_Gateway
 			'STAK' => [],
 			'STAKE' => [],
 			'TPAY' => [],
+			'XEM' => [],
 			'XLR' => [],
 			'XVG' => [],
 			'WRD1' => [],
