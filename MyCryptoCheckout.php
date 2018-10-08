0bd55bf9055715342f24b2f62ca6d60e837d0da7
Edward Plainview <edward@plainview.se>
2018-10-08 18:34:00 +0200

    v2.21

--- MyCryptoCheckout.php
+++ MyCryptoCheckout.php
@@ -7,7 +7,7 @@ Description:	Cryptocurrency payment gateway using the MyCryptoCheckout.com servi
 Plugin Name:	MyCryptoCheckout
 Plugin URI:		https://mycryptocheckout.com
 Text Domain:	mycryptocheckout
-Version:		2.20
+Version:		2.21
 */
 
 namespace mycryptocheckout
@@ -55,7 +55,7 @@ namespace mycryptocheckout
 
 namespace
 {
-	define( 'MYCRYPTOCHECKOUT_PLUGIN_VERSION', 2.20 );
+	define( 'MYCRYPTOCHECKOUT_PLUGIN_VERSION', 2.21 );
 	/**
 		@brief		Return the instance of ThreeWP Broadcast.
 		@since		2014-10-18 14:48:37
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://mycryptocheckout.com
 License: GPLv3
 Requires at least: 4.9
 Requires PHP: 5.4
-Stable tag: 2.20
+Stable tag: 2.21
 Tags: bitcoin, ethereum, cryptocurrency, gateway, woocommerce
 Tested up to: 4.9.8
 
@@ -167,6 +167,8 @@ If your server cannot be reached by the API server this plugin will not function
 12. Donations widget
 
 == Changelog ==
+
+= 2.21 20181006 =
 
 * New: HD wallet support for Bitcoin Cash BCH
 * New: HD wallet support for Litecoin LTC
