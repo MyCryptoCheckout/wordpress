ebd5fbb370a947bb747ef4286bd305ffe3165abc
uniquelylost <jsneider24@gmail.com>
2019-08-13 16:25:58 -0400

    update npm, dependencies, and swap autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,32 +13,32 @@ module.exports = function(grunt) {
       			}
     		}
 		},
-		autoprefixer:{
-			options: {
-  				browsers: ['last 10 versions']
-			},
-    		dist:{
-        		files:{
-          			'src/static/css/mycryptocheckout.css':'src/static/css/mycryptocheckout.css'
-        		}
-      		}
-    	},
+    	postcss: {
+  			options: {
+    			processors: [
+      				require('autoprefixer')({overrideBrowserslist: ['last 10 version']})
+    			]
+  			},
+  			dist: {
+    			src: 'src/static/css/mycryptocheckout.css'
+  			}
+		},
 		watch: {
   			scripts: {
 				files: ['src/static/css/scss/*.scss'],
-				tasks: ['sass', 'autoprefixer'],
+				tasks: ['sass', 'postcss'],
   			},
 		},
 	});
 
 	// Load plugins
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('@lodder/grunt-postcss');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Register Tasks
 	grunt.registerTask('compile-sass', ['sass']);
-	grunt.registerTask('prefix-css', ['autoprefixer']);
+	grunt.registerTask('prefix-css', ['postcss']);
 	
 	// Run All Tasks
 	grunt.registerTask('all', ['compile-sass', 'prefix-css']);
