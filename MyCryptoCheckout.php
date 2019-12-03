0ef7e83d52d5840b368d835b7df38aef0ea4cad0
Edward Plainview <edward@plainview.se>
2019-12-03 18:39:08 +0100

    v2.71

--- MyCryptoCheckout.php
+++ MyCryptoCheckout.php
@@ -7,7 +7,7 @@ Description:	Cryptocurrency payment gateway using the MyCryptoCheckout.com servi
 Plugin Name:	MyCryptoCheckout
 Plugin URI:		https://mycryptocheckout.com
 Text Domain:	mycryptocheckout
-Version:		2.70
+Version:		2.71
 */
 
 namespace mycryptocheckout
@@ -62,7 +62,7 @@ namespace mycryptocheckout
 
 namespace
 {
-	define( 'MYCRYPTOCHECKOUT_PLUGIN_VERSION', 2.70 );
+	define( 'MYCRYPTOCHECKOUT_PLUGIN_VERSION', 2.71 );
 	/**
 		@brief		Return the instance of ThreeWP Broadcast.
 		@since		2014-10-18 14:48:37
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://mycryptocheckout.com
 License: GPLv3
 Requires at least: 4.9
 Requires PHP: 5.6
-Stable tag: 2.70
+Stable tag: 2.71
 Tags: bitcoin, ethereum, cryptocurrency, gateway, woocommerce
 Tested up to: 5.3
 
@@ -258,6 +258,8 @@ The following plugins prevent MyCryptoCheckout from working correctly:
 - Plugin Organizer by Jeff Sterup. Deactivate plugin.
 
 == Changelog ==
+
+= 2.71 20191203 =
 
 * New currency: ARightMesh RMESH
 * New currency: ATBC Mart Token TMT
