3ddd92b4b885fff95eb5d001c9c1f542814006a6
uniquelylost <jsneider24@gmail.com>
2020-02-20 12:54:35 -0500

    Update DigibyteNetwork.php

--- src/currencies/DigibyteNetwork.php
+++ src/currencies/DigibyteNetwork.php
@@ -18,51 +18,66 @@ use BitWasp\Bitcoin\Network\Network;
 use BitWasp\Bitcoin\Script\ScriptType;
 class DigibyteNetwork extends Network
 {
-    /**
-     * {@inheritdoc}
-     * @see Network::$base58PrefixMap
-     */
-    protected $base58PrefixMap = [
-		// https://github.com/SmartCash/Core-Smart/blob/master/src/chainparams.cpp#L165 - L167 on 5/5/19. Convert numbers from decimal to hex.
-        self::BASE58_ADDRESS_P2PKH => "1E",
-        self::BASE58_ADDRESS_P2SH => "3F",
-        self::BASE58_WIF => "80",
-    ];
+	/**
+	 * {@inheritdoc}
+	 * @see Network::$base58PrefixMap
+	 *
+	 * @since 2/20/20
+	 *
+	 * - https://github.com/digibyte/digibyte/blob/master/src/chainparams.cpp#L231
+	 * - Convert numbers from decimal to hex.
+	 */
+	protected $base58PrefixMap = [
+		self::BASE58_ADDRESS_P2PKH => "1E",
+		self::BASE58_ADDRESS_P2SH => "3F",
+		self::BASE58_WIF => "80",
+	];
 
-    /**
-     * {@inheritdoc}
-     * @see Network::$bech32PrefixMap
-     */
-    protected $bech32PrefixMap = [
-        self::BECH32_PREFIX_SEGWIT => "dgb",
-    ];
+	/**
+	 * {@inheritdoc}
+	 * @see Network::$bech32PrefixMap
+	 */
+	protected $bech32PrefixMap = [
+		self::BECH32_PREFIX_SEGWIT => "dgb",
+	];
 
-    /**
-     * {@inheritdoc}
-     * @see Network::$bip32PrefixMap
-     */
-    protected $bip32PrefixMap = [
-		// https://github.com/SmartCash/Core-Smart/blob/master/src/chainparams.cpp#L168 - L169 on 5/5/19. Drop 0x's and concatenate the numbers.
-        self::BIP32_PREFIX_XPUB => "0488b21e",
-        self::BIP32_PREFIX_XPRV => "0488ade4",
-    ];
-    /**
-     * {@inheritdoc}
-     * @see Network::$bip32ScriptTypeMap
-     */
+	/**
+	 * {@inheritdoc}
+	 * @see Network::$bip32PrefixMap
+	 *
+	 * @since 2/20/20
+	 *
+	 * - https://github.com/digibyte/digibyte/blob/master/src/chainparams.cpp#L236
+	 * - Drop 0x's and concatenate the numbers
+	 */
+	protected $bip32PrefixMap = [
+		self::BIP32_PREFIX_XPUB => "0488b21e",
+		self::BIP32_PREFIX_XPRV => "0488ade4",
+	];
+
+	/**
+	 * {@inheritdoc}
+	 * @see Network::$bip32ScriptTypeMap
+	 */
 	protected $bip32ScriptTypeMap = [
-        self::BIP32_PREFIX_XPUB => ScriptType::P2PKH,
-        self::BIP32_PREFIX_XPRV => ScriptType::P2PKH,
-    ];
-    /**
-     * {@inheritdoc}
-     * @see Network::$signedMessagePrefix
-     */
-    protected $signedMessagePrefix = "Digibyte Signed Message";
-    /**
-     * {@inheritdoc}
-     * @see Network::$p2pMagic
-     */
-	 // https://github.com/SmartCash/Core-Smart/blob/master/src/chainparams.cpp#L136 - L139 on 5/5/19. Drop 0x's and concatenate in reverse (ascending) order.
+		self::BIP32_PREFIX_XPUB => ScriptType::P2PKH,
+		self::BIP32_PREFIX_XPRV => ScriptType::P2PKH,
+	];
+
+	/**
+	 * {@inheritdoc}
+	 * @see Network::$signedMessagePrefix
+	 */
+	protected $signedMessagePrefix = "Digibyte Signed Message";
+
+	/**
+	 * {@inheritdoc}
+	 * @see Network::$p2pMagic
+	 *
+	 * @since 2/20/20
+	 *
+	 * - https://github.com/digibyte/digibyte/blob/master/src/chainparams.cpp#L208
+	 * - Drop 0x's and concatenate in reverse (ascending) order.
+	 */
 	protected $p2pMagic = "dab6c3fa";
 }
