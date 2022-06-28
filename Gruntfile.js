4d243737c2f41643f167a17b77f0df702f60d576
Jesse S <3428918+jjs2484@users.noreply.github.com>
2022-06-28 15:40:58 -0400

    move 3rd party scripts to NPM, upgrade web3, add bignumber.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,17 +25,46 @@ module.exports = function(grunt) {
     			src: 'src/static/css/mycryptocheckout.css'
   			}
 		},
+		copy: {
+			main: {
+				files: [
+					// copies web3.js file
+					{ 
+						expand: true,
+						flatten: true,
+						src: ['node_modules/web3/dist/web3.min.js'], 
+						dest: 'src/static/js/',
+						filter: 'isFile'
+					},
+					// copies bignumber.js file
+					{ 
+						expand: true,
+						flatten: true,
+						src: ['node_modules/bignumber.js/bignumber.js'], 
+						dest: 'src/static/js/js.d/',
+						filter: 'isFile',
+						// Rename the file to include '40.' prefix 
+						rename: function (dest, matchedSrcPath) {
+							if (matchedSrcPath.substring(0, 1) !== '4') {
+								return dest + '40.' + matchedSrcPath;
+							}
+						}
+					},
+				],
+			},
+		},
 	});
 
 	// Load plugins
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('@lodder/grunt-postcss');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
 	// Register Tasks
 	grunt.registerTask('compile-sass', ['sass']);
 	grunt.registerTask('prefix-css', ['postcss']);
 	
 	// Run All Tasks
-	grunt.registerTask('all', ['compile-sass', 'prefix-css']);
+	grunt.registerTask('all', ['compile-sass', 'prefix-css', 'copy']);
 
 };
