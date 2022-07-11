46f23975a3352a783da605d1770ddefe8d5b0fa6
Jesse S <3428918+jjs2484@users.noreply.github.com>
2022-07-11 16:33:26 -0400

    add clipboard.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,6 +50,20 @@ module.exports = function(grunt) {
 							}
 						}
 					},
+					// copies clipboard.js file
+					{ 
+						expand: true,
+						flatten: true,
+						src: ['node_modules/clipboard/dist/clipboard.js'], 
+						dest: 'src/static/js/js.d/',
+						filter: 'isFile',
+						// Rename the file to include '40.' prefix 
+						rename: function (dest, matchedSrcPath) {
+							if (matchedSrcPath.substring(0, 1) !== '4') {
+								return dest + '40.' + matchedSrcPath;
+							}
+						}
+					},
 				],
 			},
 		},
