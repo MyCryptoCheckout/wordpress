5a6b44f5e3cfd475db7188e447ae60542a6a8638
Jesse S <3428918+jjs2484@users.noreply.github.com>
2024-05-03 10:18:41 -0700

    add solana web3

--- src/misc_methods_trait.php
+++ src/misc_methods_trait.php
@@ -63,6 +63,7 @@ trait misc_methods_trait
 	public function enqueue_web3_js()
 	{
 		wp_enqueue_script( 'mycryptocheckout-web3', MyCryptoCheckout()->paths( 'url' ) . '/src/static/js/web3.min.js', [ 'jquery' ], MYCRYPTOCHECKOUT_PLUGIN_VERSION );
+		wp_enqueue_script( 'mycryptocheckout-web3-sol', MyCryptoCheckout()->paths( 'url' ) . '/src/static/js/index.iife.min.js', [ 'jquery' ], MYCRYPTOCHECKOUT_PLUGIN_VERSION );
 	}
 
 	/**
