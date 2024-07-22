0414188250c079702907d2066d0b0c4aaea19f2a
Edward Plainview <edward@plainview.se>
2024-07-22 09:30:35 +0200

    v2.137

--- MyCryptoCheckout.php
+++ MyCryptoCheckout.php
@@ -7,7 +7,7 @@ Description:		Cryptocurrency payment gateway for WooCommerce and Easy Digital Do
 Plugin Name:		MyCryptoCheckout
 Plugin URI:			https://mycryptocheckout.com
 Text Domain:		mycryptocheckout
-Version:			2.136
+Version:			2.137
 WC tested up to:	9.1.2
 */
 
@@ -71,7 +71,7 @@ namespace mycryptocheckout
 
 namespace
 {
-	define( 'MYCRYPTOCHECKOUT_PLUGIN_VERSION', 2.136 );
+	define( 'MYCRYPTOCHECKOUT_PLUGIN_VERSION', 2.137 );
 	/**
 		@brief		Return the instance of MCC.
 		@since		2014-10-18 14:48:37
