85b4b0f11a6364618d9f9555553a13ac7e5381ee
Edward Plainview <edward@plainview.se>
2026-01-05 07:58:09 +0100

    Remove wallet sort init

--- src/static/js/js.d/98.init.js
+++ src/static/js/js.d/98.init.js
@@ -6,9 +6,3 @@ $( 'form.plainview_form_auto_tabs' ).plainview_form_auto_tabs();
 $( '.mcc_donations' ).mycryptocheckout_donations_javascript();
 
 $( 'form#currencies' ).mycryptocheckout_new_currency();
-
-/**
-	@brief		Make the wallets sortable.
-	@since		2018-10-17 17:38:58
-**/
-$( 'table.currencies tbody' ).mycryptocheckout_sort_wallets();
